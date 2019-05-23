0e9a7e090afdc086c057fe28a01b4abf7c5eeefd
Wiktor <adanos53@gmail.com>
2019-05-23 21:55:02 +0200

    dodanie heroku.yml

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #flask run --host=0.0.0.0
-python ./manage.py recreate_db
+python manage.py recreate_db
 python manage.py add_fake_data -n 100
-gunicorn --bind 0.0.0.0:5000 manage:app
+gunicorn --bind 0.0.0.0:$PORT  manage:app
