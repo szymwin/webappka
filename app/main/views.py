4d7bc7c8a1ebbcd0e593d3c20b9aa7c0cd951191
Szymon <szymon.win96@gmail.com>
2019-05-27 23:02:21 +0200

    pierwsze proby z wykresami

--- app/main/views.py
+++ app/main/views.py
@@ -26,35 +26,41 @@ def show_raw():
 def show_result():
     fd_list = db.session.query(Formdata).all()
 
-    # Some simple statistics for sample questions
-    satisfaction = []
-    q1 = []
-    q2 = []
+    wt = 0
+    wn = 0
+    wtp = 0
+    nt = 0
+    nn = 0
+    ntp = 0
+    tt = 0
+    tn = 0
+    ttp = 0
+
     for el in fd_list:
-        satisfaction.append(int(el.satisfaction))
-        q1.append(int(el.q1))
-        q2.append(int(el.q2))
+        if el.faith == 1:
+            if el.support == 1:
+                wt += 1
+            elif el.support == 2:
+                wn +=1
+            elif el.support == 3:
+                wtp += 1
+        if el.faith == 2:
+            if el.support == 1:
+                nt += 1
+            elif el.support == 2:
+                nn += 1
+            elif el.support == 3:
+                ntp += 1
+        if el.faith == 3:
+            if el.support == 1:
+                tt += 1
+            elif el.support == 2:
+                tn += 1
+            elif el.support == 3:
+                ttp += 1
 
-    if len(satisfaction) > 0:
-        mean_satisfaction = statistics.mean(satisfaction)
-    else:
-        mean_satisfaction = 0
+    data = [['Tak', wt], ['Nie', wn], ['Trudno powiedziec', wtp]]
 
-    if len(q1) > 0:
-        mean_q1 = statistics.mean(q1)
-    else:
-        mean_q1 = 0
-
-    if len(q2) > 0:
-        mean_q2 = statistics.mean(q2)
-    else:
-        mean_q2 = 0
-
-    # Prepare data for google charts
-    data = [['Satisfaction', mean_satisfaction], ['Python skill', mean_q1], ['Flask skill', mean_q2]]
-
-
-    data2 = [['Satisfaction', mean_satisfaction], ['Python skill', mean_q1], ['Flask skill', mean_q2]]
     return render_template('main/result.html', data=data)
 
 
